4a44a188911530eb5f7a5a4fc9ce2f57ab707051
WhaleisaJoy <afterlifelineonomega@gmail.com>
2020-10-21 21:20:39 +0300

    Доработки декоратива + js

--- js/script.js
+++ js/script.js
@@ -1,66 +1,8 @@
-// let menuCatalog = document.querySelector('.menu-catalog');
-// let submenuCatalog = document.querySelector('.submenu-catalog');
-
-let mainHeaderButton = document.querySelectorAll('.main-header-button');
-let popupAll = document.querySelectorAll('.popup');
-
-let pageBody = document.querySelector('.page-body');
-let contactsButton = document.querySelector('.contacts-button');
-let modalFeedback = document.querySelector('.overlay-feedback');
-let modalCloseButton = document.querySelector('.modal-close');
-let overlay = document.querySelector('.overlay');
-
-// menuCatalog.addEventListener("mouseenter", function(){
-//     this.classList.add('open');
-//     // submenuCatalog.classList.toggle('open');
-// });
-
-// submenuCatalog.onmouseleave = function(){
-//     // submenuCatalog.classList.toggle('open');
-// }
-
-// menuCatalog.onmouseleave = function(){
-//     this.classList.remove('open');
-//     // submenuCatalog.classList.toggle('open');
-// }
-
-/*menuCatalog.addEventListener("mouseenter", function(){
-    menuCatalog.classList.add('open');
-    submenuCatalog.classList.add('open');
-});
-
-submenuCatalog.addEventListener("mouseleave", function(){
-    menuCatalog.classList.remove('open');
-    submenuCatalog.classList.remove('open');
-});*/
-
-// main header button
-mainHeaderButton.forEach(function(elem){
-    elem.addEventListener("mouseover", function(){
-        mainHeaderButton.forEach(function(elem){
-            elem.classList.remove('open');
-        });
-
-        popupAll.forEach(function(elem){
-            elem.classList.remove('open');
-        });
-
-        this.classList.add('open');
-        let parentElem = this.parentNode;      
-        let popup = parentElem.querySelector('.popup');
-        popup.classList.add('open');   
-    });
-});
-
-popupAll.forEach(function(elem){
-    elem.addEventListener("mouseleave", function(){
-        let parentElem = this.parentNode;      
-        let curButton = parentElem.querySelector('.main-header-button');
-        curButton.classList.remove('open');
-
-        this.classList.remove('open');
-    });  
-});
+const pageBody = document.querySelector('.page-body');
+const contactsButton = document.querySelector('.contacts-button');
+const modalFeedback = document.querySelector('.overlay-feedback');
+const modalCloseButton = document.querySelector('.modal-close');
+const overlay = document.querySelector('.overlay');
 
 // modal feedback
 contactsButton.addEventListener("click", function(evt){
